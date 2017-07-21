8fa8170570604cf5259a4bf3ad915ac7da384442
Michael J. Simons <michael@simons.ac>
2017-07-21 17:23:39 +0300

    Register with eureka

--- src/main/java/de/bootifultodos/configserver/ConfigServerApplication.java
+++ src/main/java/de/bootifultodos/configserver/ConfigServerApplication.java
@@ -18,6 +18,7 @@ package de.bootifultodos.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Michael J. Simons, 2017-07-15
@@ -25,6 +26,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SuppressWarnings({"checkstyle:designforextension"})
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(final String... args) {
